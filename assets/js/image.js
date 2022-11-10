3c5c6ef1573f16bbe439a1e7cfd5d2669011a612
Alan Yeverino <lanyev@gmail.com>
2022-11-10 17:35:20 -0600

    version1

--- assets/js/image.js
+++ assets/js/image.js
@@ -1,22 +1,21 @@
 $(document).ready(function () {
   $(this).scrollTop(0);
+  const images = [
+    "/assets/img/alanCard0.png",
+    "/assets/img/alanCard1.png",
+    "/assets/img/alanCard2.png",
+    "/assets/img/alanCard3.png",
+  ];
+
+  // let index = 0;
+  // const imgElement = document.querySelector("#banner-load");
+
+  // function change() {
+  //   imgElement.src = images[index];
+  //   index > 1 ? (index = 0) : index++;
+  // }
+
+  // window.onload = function () {
+  //   setInterval(change, 1000);
+  // };
 });
-
-// const images = [
-//   "/assets/img/alanCard0.png",
-//   "/assets/img/alanCard1.png",
-//   "/assets/img/alanCard2.png",
-//   "/assets/img/alanCard3.png",
-// ];
-
-// let index = 0;
-// const imgElement = document.querySelector("#banner-load");
-
-// function change() {
-//   imgElement.src = images[index];
-//   index > 1 ? (index = 0) : index++;
-// }
-
-// window.onload = function () {
-//   setInterval(change, 1000);
-// };
